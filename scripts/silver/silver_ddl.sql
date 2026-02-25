61578dae5a81c7c079ee7a5ef8da857256624987
Shaik Thajuddhin <141851715+thaju-cse@users.noreply.github.com>
2026-02-25 08:00:52 +0530

    Update silver_ddl.sql

--- scripts/silver/silver_ddl.sql
+++ scripts/silver/silver_ddl.sql
@@ -1,3 +1,13 @@
+/*
+===============================================================================
+DDL Script: Create Silver Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'silver' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
 create or replace procedure silver.ddl_silver() 
 language plpgsql
 as $$
