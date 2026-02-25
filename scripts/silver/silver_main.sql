0409fcdf4b8952d241106e0e3bebc1d5d794e13f
Shaik Thajuddhin <141851715+thaju-cse@users.noreply.github.com>
2026-02-25 08:03:20 +0530

    Update silver_main.sql

--- scripts/silver/silver_main.sql
+++ scripts/silver/silver_main.sql
@@ -1,2 +1,17 @@
+/*
+===============================================================================
+Calling Stored Procedure: Load Silver Layer (Bronze -> Silver)
+===============================================================================
+Script Purpose:
+    Calling ddl_silver() and load_silver procedures for batch processing.		
+Parameters:
+    None. 
+	  This stored procedure does not accept any parameters or return any values.
+Usage Example:
+  call silver.ddl_silvr();  
+  call silver.load_silver();
+    
+===============================================================================
+*/
 call silver.ddl_silver();
 call silver.load_silver();
