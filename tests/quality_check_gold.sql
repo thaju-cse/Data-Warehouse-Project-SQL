d6f88783fd98acc7b984de087a6ca43a32a1966f
Shaik Thajuddhin <141851715+thaju-cse@users.noreply.github.com>
2026-02-25 08:23:04 +0530

    Update quality_check_gold.sql

--- tests/quality_check_gold.sql
+++ tests/quality_check_gold.sql
@@ -1,1 +1,53 @@
 
+/*
+===============================================================================
+Quality Checks
+===============================================================================
+Script Purpose:
+    This script performs quality checks to validate the integrity, consistency, 
+    and accuracy of the Gold Layer. These checks ensure:
+    - Uniqueness of surrogate keys in dimension tables.
+    - Referential integrity between fact and dimension tables.
+    - Validation of relationships in the data model for analytical purposes.
+
+Usage Notes:
+    - Investigate and resolve any errors found during the checks.
+===============================================================================
+*/
+
+-- ==============================
+-- Checking 'gold.dim_customers'
+-- ==============================
+-- Check for Uniqueness of Customer Key in gold.dim_customers
+-- Expectation: No results 
+
+SELECT 
+    customer_dimension_id,
+    COUNT(*) AS duplicate_count
+FROM gold.dim_customers
+GROUP BY customer_dimension_id
+HAVING COUNT(*) > 1;
+
+-- ============================
+-- Checking 'gold.dim_products'
+-- ============================
+-- Check for Uniqueness of Product Key in gold.dim_products
+-- Expectation: No results 
+SELECT 
+    product_dimension_id,
+    COUNT(*) AS duplicate_count
+FROM gold.dim_products
+GROUP BY product_dimension_id
+HAVING COUNT(*) > 1;
+
+-- ==========================
+-- Checking 'gold.fact_sales'
+-- ==========================
+-- Check the data model connectivity between fact and dimensions
+SELECT * 
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c
+ON c.customer_key = f.customer_dimension_id
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_dimension_id
+WHERE p.product_dimension_id IS NULL OR c.customer_dimension_id IS NULL  
